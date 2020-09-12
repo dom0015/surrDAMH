78c31f9d9684f06c26982110c7ab3e0c9b55b32e
dom0015 <dom0015@vsb.cz>
2020-09-12 13:44:57 +0200

    Img Alg2020

--- test_Algoritmy2020.py
+++ test_Algoritmy2020.py
@@ -12,8 +12,8 @@ import matplotlib.pyplot as plt
 
 from configuration import Configuration
 C = Configuration()
-problem_name = "GRF_24to10_05"
-# problem_name = "GRF_double24to10_start1"
+# problem_name = "GRF_24to10_05"
+problem_name = "GRF_24to40_02_unlimited"
 # problem_name = C.problem_name
 
 S = aa.Samples()
@@ -37,40 +37,40 @@ tmp = int(min(S.length)*0.1)
 S.plot_average_reverse(parameters_disp = [0,1,2,23])
 
 # common color scale
-S.plot_mean_and_std_grf_rotated(chains_disp = [0])
+S.plot_mean_and_std_grf_rotated()#(chains_disp = [0])
 
-# 32 parameters:
-C = Configuration()
-problem_name = "GRF_24to10_06_unlimited_32par"
-S = aa.Samples()
-N = 8
-S.load_notes('saved_samples/' + problem_name,N)
-S.load_MH('saved_samples/' + problem_name)
-S.remove_chains(range(N,2*N)) # keep only DAMH chains
-S.calculate_properties()
-# common color scale
-S.plot_mean_and_std_grf_rotated(chains_disp = [0])
-
-
-# 40 parameters:
-C = Configuration()
-problem_name = "GRF_24to10_06_unlimited_40par"
-S = aa.Samples()
-N = 8
-S.load_notes('saved_samples/' + problem_name,N)
-S.load_MH('saved_samples/' + problem_name)
-S.remove_chains(range(N,2*N)) # keep only DAMH chains
-S.calculate_properties()
-# common color scale
-S.plot_mean_and_std_grf_rotated(chains_disp = [0])
-
-
-# problem_name = "GRF_24to10_onlyMH"
+# # 32 parameters:
+# C = Configuration()
+# problem_name = "GRF_24to10_06_unlimited_32par"
 # S = aa.Samples()
-# N = 10
+# N = 8
 # S.load_notes('saved_samples/' + problem_name,N)
 # S.load_MH('saved_samples/' + problem_name)
-# S.remove_chains(range(N)) # keep only MH chains
+# S.remove_chains(range(N,2*N)) # keep only DAMH chains
 # S.calculate_properties()
-# S.calculate_autocorr_time()
-# print("autocorr time:",np.mean(S.autocorr_time,axis=1))
+# # common color scale
+# S.plot_mean_and_std_grf_rotated(chains_disp = [0])
+
+
+# # 40 parameters:
+# C = Configuration()
+# problem_name = "GRF_24to10_06_unlimited_40par"
+# S = aa.Samples()
+# N = 8
+# S.load_notes('saved_samples/' + problem_name,N)
+# S.load_MH('saved_samples/' + problem_name)
+# S.remove_chains(range(N,2*N)) # keep only DAMH chains
+# S.calculate_properties()
+# # common color scale
+# S.plot_mean_and_std_grf_rotated(chains_disp = [0])
+
+
+# # problem_name = "GRF_24to10_onlyMH"
+# # S = aa.Samples()
+# # N = 10
+# # S.load_notes('saved_samples/' + problem_name,N)
+# # S.load_MH('saved_samples/' + problem_name)
+# # S.remove_chains(range(N)) # keep only MH chains
+# # S.calculate_properties()
+# # S.calculate_autocorr_time()
+# # print("autocorr time:",np.mean(S.autocorr_time,axis=1))
